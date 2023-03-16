0925bb1d7c0eb27c0f199aac0b688ff065475bc2
Bin Yue <byue@freewheel.tv>
2023-03-16 15:57:13 +0800

    sec: fix minimist

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    'airbnb-base',
-    'eslint:recommended'
-  ],
+  extends: ['airbnb-base', 'eslint:recommended'],
 
   // Stop ESLint from looking for a configuration file in parent folders
   root: true,
@@ -13,21 +10,23 @@ module.exports = {
   },
 
   rules: {
-    /**
-     * Style
-     */
+    'arrow-parens': ['error', 'as-needed'],
     'comma-dangle': ['error', 'never'],
-
-    /**
-     * ES 6
-     */
-    'max-len': ['error', 120, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true
-    }],
+    'function-paren-newline': ['error', 'multiline-arguments'],
+    'max-len': [
+      'error',
+      120,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    'prefer-destructuring': 0,
+    'no-param-reassign': 0,
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     'no-var': 'error'
   }
